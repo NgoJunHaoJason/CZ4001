a3244ed50d77e6078643683e3c4b0c132cd71bb0
Jason <c170127@e.ntu.edu.sg>
2020-02-02 21:49:00 +0800

    refactor PlayerController

--- Hunting/Assets/Scripts/PlayerController.cs
+++ Hunting/Assets/Scripts/PlayerController.cs
@@ -3,12 +3,15 @@
 
 /**
 *   references:
-*   https://codingchronicles.com/unity-vr-development/day-8-creating-character-first-person-shooter
+*   https://codingchronicles.com/unity-vr-development/
+day-8-creating-character-first-person-shooter
 *   https://youtu.be/blO039OzUZc
 */
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
+    # region Serialize Fields 
+
     [SerializeField]
     private int movementSpeed = 3;
 
@@ -22,43 +25,59 @@ public class PlayerController : MonoBehaviour
     private int cameraVerticalSensitivity = 3;
 
     [SerializeField]
+
+    # endregion
+
+    # region Fields
+
     private int cameraSmoothing = 2;
 
-    private Rigidbody playerRigidbody;
-    private Camera playerCamera;
+    private Rigidbody playerRigidbody = null;
+
+    private Camera playerCamera = null;
 
     private Vector2 cameraLook = new Vector2();
     private Vector2 smoothVector = new Vector2();
 
+    // prevents player from jumping while already in the air
     private bool isGrounded = true;
+
+    # endregion
+
+    # region MonoBehaviour Methods
 
     void Awake()
     {
         playerRigidbody = GetComponent<Rigidbody>();
         playerCamera = GetComponentInChildren<Camera>();
+
+        if (playerCamera == null && Debug.isDebugBuild)
+            Debug.LogError("No Camera is attached to the Player");
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         Move();
         CameraLook();
     }
 
+    # endregion
+
+    # region Private Methods
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Terrain"))
-        {
             isGrounded = true;
-        }
     }
 
-    void Move()
+    private void Move()
     {
         float horizontalAxis = Input.GetAxis("Horizontal");
         float verticalAxis = Input.GetAxis("Vertical");
 
-        Vector3 movement = horizontalAxis * transform.right + verticalAxis * transform.forward;
+        Vector3 movement = horizontalAxis * transform.right + 
+            verticalAxis * transform.forward;
 
         movement = movement.normalized * movementSpeed * Time.deltaTime;
 
@@ -71,7 +90,8 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void CameraLook()
+    // should place this method directly in Camera game object
+    private void CameraLook()
     {
         float mouseX = Input.GetAxisRaw("Mouse X");
         float mouseY = Input.GetAxisRaw("Mouse Y");
@@ -87,8 +107,13 @@ public class PlayerController : MonoBehaviour
         cameraLook += smoothVector;
         cameraLook.y = Mathf.Clamp(cameraLook.y, -70, 70);
 
-        playerCamera.transform.localRotation = Quaternion.AngleAxis(-cameraLook.y, Vector3.right);
+        playerCamera.transform.localRotation = Quaternion.AngleAxis(
+            -cameraLook.y, 
+            Vector3.right
+        );
 
         transform.rotation = Quaternion.AngleAxis(cameraLook.x, transform.up);
     }
+
+    # endregion
 }
