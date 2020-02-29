f92cea5c41f652fdf6849e067df1fb5f21a33985
Jingherng <jingherng2010@gmail.com>
2020-03-01 04:23:37 +0800

    Finished Day Night Cycle

--- Hunting/Assets/Scripts/GameManager/SunController.cs
+++ Hunting/Assets/Scripts/GameManager/SunController.cs
@@ -10,15 +10,29 @@ public class SunController : MonoBehaviour
     private float dayTime = 60;
 
     [SerializeField]
-    private float dimFactor = 3;
+    private float currentTimeOfDay = 0.5f;
+
+    public float secondsInFullDay = 120f;
+    public float timeMultiplier = 0.5f;
+
+    public MeshRenderer cloud;
     
     # endregion
 
     # region Fields
 
-    private float moveAngle = 0;
 
-    private new Light light = null;
+    private new Light sun;
+    private new float sunInitialIntensity;
+
+    private new Material high = null;
+
+    private new float r = 1f;
+    private new float g = 1f;
+    private new float b = 1f;
+
+
+    public System.Collections.Generic.List<Color> colors = new System.Collections.Generic.List<Color>();
 
     # endregion
 
@@ -26,18 +40,64 @@ public class SunController : MonoBehaviour
 
     void Start()
     {
-        if (dayTime != 0)
-            moveAngle = 60/dayTime;
         
-        light = GetComponent<Light>();
-        light.intensity = 1;
+        sun = GetComponent<Light>();
+
+        sunInitialIntensity = sun.intensity;
+
+        high = cloud.materials[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.up * Time.deltaTime * moveAngle);
-        light.color += (Color.red/(dayTime * dimFactor)) * Time.deltaTime;
+
+        UpdateSun();
+        
+        currentTimeOfDay += (Time.deltaTime / secondsInFullDay) * timeMultiplier;
+ 
+        if (currentTimeOfDay >= 1) {
+            currentTimeOfDay = 0;
+        }
+
+    }
+
+    void UpdateSun() {
+        sun.transform.localRotation = Quaternion.Euler((currentTimeOfDay * 360f) - 90, 170, 0);
+ 
+        float intensityMultiplier = 1;
+        if (currentTimeOfDay >= 0.8f || currentTimeOfDay <= 0.2f)  // Night time
+        {
+        	r = 0f;
+        	g = 0f;
+        	b = 0f;
+    	    colors.Add(new Color(r,g,b));
+        	high.SetColorArray("_CloudColor", colors);
+			colors.Clear();    		
+            intensityMultiplier = 0;
+        }
+        else if (currentTimeOfDay <= 0.7 && currentTimeOfDay >= 0.3) // Day time 
+        {
+        	r = 1f;
+        	g = 1f;
+        	b = 1f;
+    	    colors.Add(new Color(r,g,b));
+        	high.SetColorArray("_CloudColor", colors);
+			colors.Clear();   
+            intensityMultiplier = Mathf.Clamp01((currentTimeOfDay - 0.23f) * (1 / 0.02f));
+        }
+        else // sunrise, sunset
+        {
+        	r = 0.9150943f;
+        	g = 0.5568262f;
+        	b = 0.5568262f;
+    	    colors.Add(new Color(r,g,b));
+        	high.SetColorArray("_CloudColor", colors);
+			colors.Clear();   
+            intensityMultiplier = Mathf.Clamp01(1 - ((currentTimeOfDay - 0.73f) * (1 / 0.02f)));
+        }
+ 
+        sun.intensity = sunInitialIntensity * intensityMultiplier;
     }
 
     # endregion
